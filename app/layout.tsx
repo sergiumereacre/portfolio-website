1a61308bc2b4005be3f9094f22d4e89fadbaba4c
sergiu <sergiumer@gmail.com>
2024-01-13 16:08:37 +0000

    Added robots.txt and the json-ld stuff.
    Signed-off-by: sergiu <sergiumer@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,26 @@ export const metadata: Metadata = {
   description: 'Explore the distinguished portfolio of Sergiu Mereacre, an embodiment of my professional journey, expertise, and passions. Witness a carefully curated collection of my accomplishments, skills, and experiences, leaving a lasting impression of who I am.',
 }
 
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "Sergiu Mereacre",
+  "jobTitle": "Software Developer",
+  "url": "https://sergiu.ie",
+  "email": "contact@sergiu.ie",
+  "image": "https://sergiu.ie/profile.png",
+  "sameAs": [
+    "https://www.linkedin.com/in/sergiumereacre/",
+  ],
+  "height": "187cm",
+  "gender": "male",
+  "alumniOf": {
+    "@type": "CollegeOrUniversity",
+    "name": "University of Limerick",
+    "sameAs": "https://www.ul.ie/"
+  },
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -23,6 +43,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body className='dark'>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
         <Providers>{children}</Providers>
         <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "bfa63409f4ec48f987d5018b749dd4c6"}'></script>
       </body>
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 export default async function sitemap() {
-    const baseUrl = 'https://www.sergiu.ie';
+    const baseUrl = 'https://sergiu.ie';
 
     return [
         { url: baseUrl, lastModified: new Date() },
